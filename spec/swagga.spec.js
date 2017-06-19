93938bfe46a1ec3521b77cf225d56ebcf26bfe61
Dayne <dayne@scoir.com>
2017-06-19 15:11:16 -0400

    clean up and specs for validation

--- spec/swagga.spec.js
+++ spec/swagga.spec.js
@@ -13,4 +13,69 @@ describe('swagga', function () {
             expect(definition.info.title).toEqual('Basic Swagger JSON Definition')
         })
     })
+        
+    describe('validateRequest()', function () {
+        it('should fail if the path is not in the api spec', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-get.yaml')
+            expect(() => {
+                swagga.validateRequest('/does/not/exist', 'GET')
+            }).toThrowError('Unable to find \'/does/not/exist\' in schema')
+        })
+        it('should fail if the path and method are not in the api spec', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-get.yaml')
+            expect(() => {
+                swagga.validateRequest('/pets/123', 'PUT')
+            }).toThrowError('Unable to find \'put\' on \'/pets/123\' in schema')
+        })
+        it('should fail if the body does not the match the schema definition', async () => {
+            const spy = jasmine.createSpy('exception');
+            await swagga.parseDefinition('./spec/fixtures/single-post.yaml')
+            try {
+                swagga.validateRequest('/pets/123', 'POST', {
+                    body: {}
+                })
+            } catch (e) {
+                spy(e.property, e.message);
+            }
+            expect(spy).toHaveBeenCalledWith('instance', 'is not of a type(s) array');
+        })
+        it('should fail if the body does not exist, but the schema requires one', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-post.yaml')
+            expect(() => {
+                swagga.validateRequest('/pets/123', 'POST', {})
+            }).toThrowError('Body parameter is required for \'post\' on \'/pets/123\'')
+        })
+    })
+    
+    describe('validateResponse()', function () {
+        it('should fail if the path is not in the api spec', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-get.yaml')
+            expect(() => {
+                swagga.validateResponse('/does/not/exist', 'GET')
+            }).toThrowError('Unable to find \'/does/not/exist\' in schema')
+        })
+        it('should fail if the path and method are not in the api spec', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-get.yaml')
+            expect(() => {
+                swagga.validateResponse('/pets/123', 'PUT')
+            }).toThrowError('Unable to find \'put\' on \'/pets/123\' in schema')
+        })
+        it('should fail if the path and method can not return the specified status', async () => {
+            await swagga.parseDefinition('./spec/fixtures/single-post.yaml')
+            expect(() => {
+                swagga.validateResponse('/pets/123', 'POST', 450)
+            }).toThrowError('Unable to find response code 450 for \'post\' on \'/pets/123\' in schema')
+        })
+        it('should fail if the body does not match the schema definition', async () => {
+            const spy = jasmine.createSpy('exception');
+            await swagga.parseDefinition('./spec/fixtures/single-post.yaml')
+            
+            try {
+                swagga.validateResponse('/pets/123', 'POST', 200, [])
+            } catch (e) {
+                spy(e.property, e.message);
+            }
+            expect(spy).toHaveBeenCalledWith('instance', 'is not of a type(s) object');
+        })
+    })
 })
